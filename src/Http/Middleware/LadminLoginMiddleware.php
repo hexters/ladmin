5f5bc2420c2f8d19afe4b593cab9d872722702dc
saifullah <hexters@gmail.com>
2020-03-13 14:48:10 +0700

    admin page

--- src/Http/Middleware/LadminLoginMiddleware.php
+++ src/Http/Middleware/LadminLoginMiddleware.php
@@ -15,7 +15,7 @@ class LadminLoginMiddleware {
     public function handle($request, Closure $next) {
 
         if(auth()->guard(config('ladmin.auth.guard', 'web'))->guest()) {
-            return redrect()->route('administrator.login.index');
+            return redirect()->route('administrator.login.index');
         }
 
         return $next($request);
