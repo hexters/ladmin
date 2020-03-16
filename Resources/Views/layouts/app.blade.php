719b2a1d2e1ee0f427d2e0845d1041d434fa31c1
saifullah <hexters@gmail.com>
2020-03-16 09:41:43 +0700

    layout

--- Resources/Views/layouts/app.blade.php
+++ Resources/Views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ config('app.name') }}</title>
-  <link rel="stylesheet" href="{{ asset('/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
   
@@ -13,7 +13,7 @@
     @yield('content')
   </div>
 
-  <script src="{{ asset('/app.js') }}"></script>
+  <script src="{{ asset('/js/app.js') }}"></script>
 
 </body>
 </html>
