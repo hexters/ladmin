8ee62dfcb944de49eea8918cf46c1ee6d5ea02e2
saifullah <hexters@gmail.com>
2020-03-16 09:53:13 +0700

    login

--- Resources/Views/auth/login.blade.php
+++ Resources/Views/auth/login.blade.php
@@ -10,34 +10,35 @@
         </div>
         <div class="card border-0 shadow-sm rounded-lg mt-3">
             <div class="card-body p-5">
-            <form action="" method="post">
+            <form action="{{ route('administrator.login.store') }}" method="post">
+                @csrf
                 <div class="form-group">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text bg-white border-right-0" id="basic-addon1"><i class="fas fa-envelope"></i></span>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                        <span class="input-group-text bg-white border-right-0" id="basic-addon1"><i class="fas fa-envelope"></i></span>
+                        </div>
+                        <input type="email" name="email" placeholder="Email Address" class="form-control border-left-0">
                     </div>
-                    <input type="email" name="email" placeholder="Email Address" class="form-control border-left-0">
-                </div>
                 </div>
 
                 <div class="form-group">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text bg-white border-right-0" id="basic-addon1"><i class="fas fa-lock"></i></span>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                        <span class="input-group-text bg-white border-right-0" id="basic-addon1"><i class="fas fa-lock"></i></span>
+                        </div>
+                        <input type="password" placeholder="Password" name="password" class="form-control border-left-0">
                     </div>
-                    <input type="password" placeholder="Password" name="password" class="form-control border-left-0">
-                </div>
                 </div>
 
                 <div class="form-group">
-                <input type="checkbox" name="remember" id="remember"> <label for="remember">Remember me</label>
-                <a href="" class="float-right mb-3">Forgot password ?</a>
+                    <input type="checkbox" name="remember" id="remember"> <label for="remember">Remember me</label>
+                    <a href="" class="float-right mb-3">Forgot password ?</a>
                 </div>
 
                 <div class="form-group">
-                <button class="btn btn-block btn-primary">
-                    Sign In
-                </button>
+                    <button type="submit" class="btn btn-block btn-primary">
+                        Sign In
+                    </button>
                 </div>
             </form>
             </div>
