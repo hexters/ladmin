1b673b948e7bb3bdf559a205fe319c5ab967ee0b
saifullah <hexters@gmail.com>
2020-03-16 10:52:18 +0700

    fields

--- src/Fields/Input.php
+++ src/Fields/Input.php
@@ -7,7 +7,7 @@ class Input {
   protected function options($attributes) {
     $attribute = '';
     foreach( $attributes as $attr => $val ) {
-      $attribute .= $attr . '="' . $val . '"';
+      $attribute .= $attr . '="' . $val . '" ';
     }
     return $attribute;
   }
