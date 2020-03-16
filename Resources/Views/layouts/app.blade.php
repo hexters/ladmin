cfb8976c3a6a13c54f84681c9762a182e79dba6c
saifullah <hexters@gmail.com>
2020-03-16 09:40:39 +0700

    layout

--- Resources/Views/layouts/app.blade.php
+++ Resources/Views/layouts/app.blade.php
@@ -5,10 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ config('app.name') }}</title>
+  <link rel="stylesheet" href="{{ asset('/app.css') }}">
 </head>
 <body>
   
-  @yield('content')
+  <div id="app">
+    @yield('content')
+  </div>
+
+  <script src="{{ asset('/app.js') }}"></script>
 
 </body>
 </html>
--- Resources/Views/layouts/auth.blade.php
+++ Resources/Views/layouts/auth.blade.php
@@ -5,10 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ config('app.name') }} | Login</title>
+  <link rel="stylesheet" href="{{ asset('/app.css') }}">
 </head>
 <body>
   
-  @yield('content')
+  <div id="app">
+    @yield('content')
+  </div>
 
+  <script src="{{ asset('/app.js') }}"></script>
 </body>
 </html>
