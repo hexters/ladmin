8857b6bdb38931c14a5ca04230c6be33e86cb181
saifullah <hexters@gmail.com>
2020-03-17 07:52:03 +0700

    alert

--- Resources/Views/layouts/_alert.blade.php
+++ Resources/Views/layouts/_alert.blade.php
@@ -1,5 +1,8 @@
 @if ($errors->any())
     <div class="alert alert-danger">
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
