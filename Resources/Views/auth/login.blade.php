cd0f91eff99a8f9248742b68dac5c075b80ed673
saifullah <hexters@gmail.com>
2020-03-18 14:18:20 +0700

    is invalid style

--- Resources/Views/auth/login.blade.php
+++ Resources/Views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <p><strong>Welcome!</strong> Go sign in to access administrator page.</p>
             </div>
             <x-ladmin-card class="mt-3">
-                <form action="{{ route('administrator.login.store') }}" method="post" class="mt-5">
+                <form action="{{ route('administrator.login.store') }}" method="post" class="m-5">
                     @csrf
 
                     @include('ladmin::layouts._alert')
