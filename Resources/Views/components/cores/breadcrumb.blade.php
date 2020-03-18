da2170f7ef2fc7cba815f6273684cb10ed393391
saifullah <hexters@gmail.com>
2020-03-18 17:17:47 +0700

    breadcrumb

--- Resources/Views/components/cores/breadcrumb.blade.php
+++ Resources/Views/components/cores/breadcrumb.blade.php
@@ -1,6 +1,6 @@
 <ol class="breadcrumb">
   @foreach ($items as $item)
-    <li class="breadcrumb-item {{ $loop->last() ? 'active' : null }}">
+    <li class="breadcrumb-item {{ $loop->last ? 'active' : null }}">
       <a href="{{ $item->url }}">{{ $item->name }}</a>
     </li>
   @endforeach
