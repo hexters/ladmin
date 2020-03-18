d66f53e6b2e05a8ac410a7d49d66737db5f47c4b
saifullah <hexters@gmail.com>
2020-03-18 13:02:24 +0700

    fix

--- Resources/Views/auth/login.blade.php
+++ Resources/Views/auth/login.blade.php
@@ -15,11 +15,27 @@
 
                 @include('ladmin::layouts._alert')
 
-                @foreach ($forms as $items)
-                    @foreach ($items as $item)
-                        {!! $item !!}
-                    @endforeach
-                @endforeach
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white border-right-0">
+                                <i class="fas fa-envelope"></i>
+                            </span>
+                        </div>
+                        <input type="email" name="email" value="{{ old('email') }}" class="form-control border-left-0">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-white border-right-0">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                        </div>
+                        <input type="password" name="password" class="form-control border-left-0">
+                    </div>
+                </div>
 
                 <div class="form-group">
                     <input type="checkbox" name="remember" id="remember"> <label for="remember">Remember me</label>
