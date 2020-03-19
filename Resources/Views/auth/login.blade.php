46608ca0e4d07aae9d226b628b7c1d8338177f56
saifullah <hexters@gmail.com>
2020-03-19 08:01:11 +0700

    auth

--- Resources/Views/auth/login.blade.php
+++ Resources/Views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <p><strong>Welcome!</strong> Go sign in to access administrator page.</p>
             </div>
             <x-ladmin-card class="mt-3">
-                <form action="{{ route('administrator.login.store') }}" method="post" class="my-3 mx-4">
+                <form action="{{ route('administrator.login.') }}" method="post" class="my-3 mx-4">
                     @csrf
 
                     @include('ladmin::layouts._alert')
