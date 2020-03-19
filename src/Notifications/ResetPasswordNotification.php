c922b0ae0d0541d5cf400d988746ae1074a33bd7
saifullah <hexters@gmail.com>
2020-03-19 08:37:49 +0700

    password reset

--- src/Notifications/ResetPasswordNotification.php
+++ src/Notifications/ResetPasswordNotification.php
@@ -44,7 +44,7 @@ class ResetPasswordNotification extends Notification implements ShouldQueue {
         return (new MailMessage)
                     ->subject(config('app.name') . ' - Reset Password')
                     ->line('You are receiving this email because we received a password reset request for your account.')
-                    ->action('Reset Password', url("/member/password/reset/{$this->token}?email={$this->email}"))
+                    ->action('Reset Password', route("administrator.password.reset", [$this->token, 'email' => $this->email]))
                     ->line('This password reset link will expire in 60 minutes.')
                     ->line('If you did not request a password reset, no further action is required.');
     }
