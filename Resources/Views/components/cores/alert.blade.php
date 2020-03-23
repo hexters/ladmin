1f38585b43f9b11388df467c8d1534ff81318363
saifullah <hexters@gmail.com>
2020-03-23 15:49:56 +0700

    btn

--- Resources/Views/components/cores/alert.blade.php
+++ Resources/Views/components/cores/alert.blade.php
@@ -14,6 +14,9 @@
 @foreach ($status as $state)
     @if(session()->has($state))
       <div class="alert alert-{{ $state }}">
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
         <ul>
           @foreach (session()->get($state) as $item)
             <li>{!! $item !!}</li>
