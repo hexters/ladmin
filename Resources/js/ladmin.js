c58b2d7916d03473004f583bb5c38f942f0a40f9
saifullah <hexters@gmail.com>
2020-03-23 14:45:32 +0700

    js

--- Resources/js/ladmin.js
+++ Resources/js/ladmin.js
@@ -81,7 +81,9 @@ $(function() {
   });
 });
 
-document.querySelectorAll('.dt-bootstrap4').forEach(el => {
-  let row = el.querySelectorAll('.row')[0];
-  row.querySelectorAll('div')[0].remove();
-});
+setTimeout(() => {
+  document.querySelectorAll('.dt-bootstrap4').forEach(el => {
+    let row = el.querySelectorAll('.row')[0];
+    row.querySelectorAll('div')[0].remove();
+  });
+}, 500);
