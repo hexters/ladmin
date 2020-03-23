f2282d345817e9d08831653b3f56509c31a0f7a6
saifullah <hexters@gmail.com>
2020-03-23 13:52:29 +0700

    edit form

--- Resources/Views/vendor/user/_partials/_form.blade.php
+++ Resources/Views/vendor/user/_partials/_form.blade.php
@@ -1,16 +1,16 @@
-<x-ladmin-input name="name" type="text" :value="$user->name" old="true" required="true">
+<x-ladmin-input name="name" label="Full Name" type="text" :value="$user->name" old="true" required="true">
   <x-slot name="prepend">
     <i class="fas fa-account"></i>
   </x-slot>
 </x-ladmin-input>
 
-<x-ladmin-input name="email" type="email" :value="$user->email" old="true" required="true">
+<x-ladmin-input name="email" label="E-Mail Address" type="email" :value="$user->email" old="true" required="true">
   <x-slot name="prepend">
     <i class="fas fa-envelope"></i>
   </x-slot>
 </x-ladmin-input>
 
-<x-ladmin-input name="password" type="password">
+<x-ladmin-input name="password" label="Password" type="password">
   <x-slot name="prepend">
     <i class="fas fa-lock"></i>
   </x-slot>
--- Resources/Views/vendor/user/edit.blade.php
+++ Resources/Views/vendor/user/edit.blade.php
@@ -2,16 +2,18 @@
 @section('title', 'Edit User')
 @section('content')
     
-  <form action="{{ route('administrator.account.admin.edit', $user->id) }}" method="post">
-    @csrf 
-    @method('PUT')
-    @include('vendor.ladmin.user._partials._form', ['user' => $user])
-
-    <div class="text-right">
-      <button type="submit" class="btn btn-primary">
-        Update User
-      </button>
-    </div>
-  </form>
+  <x-ladmin-card>
+    <form action="{{ route('administrator.account.admin.edit', $user->id) }}" method="post">
+      @csrf 
+      @method('PUT')
+      @include('vendor.ladmin.user._partials._form', ['user' => $user])
+  
+      <div class="text-right">
+        <button type="submit" class="btn btn-primary">
+          Update User
+        </button>
+      </div>
+    </form>
+  </x-ladmin-card>
 
 @endsection
