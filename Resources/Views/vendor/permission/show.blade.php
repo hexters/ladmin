a550568c914c2895c1559314b5ea5604a1259aa1
saifullah <hexters@gmail.com>
2020-03-24 08:18:38 +0700

    dummy

--- Resources/Views/vendor/permission/show.blade.php
+++ Resources/Views/vendor/permission/show.blade.php
@@ -2,6 +2,15 @@
 @section('title', 'Assign Permission')
 @section('content')
     
-  
+  <x-ladmin-card>
+    <form action="" method="post">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quas repudiandae facere voluptas tenetur at ad sit incidunt? Hic, magni soluta voluptatem cupiditate quo laudantium consequuntur sint tempora laborum ex!
+      <x-slot name="footer">
+        <div class="text-right">
+          <button type="submit" class="btn btn-primary">Save Permission</button>
+        </div>
+      </x-slot>
+    </form>
+  </x-ladmin-card>
 
 @endsection
