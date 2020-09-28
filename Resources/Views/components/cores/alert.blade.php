6fa7bbe65a95f0ebc0a66cbce177edb555f1e1e9
Saifullah <hexters@gmail.com>
2020-09-28 13:48:41 +0700

    Update alert.blade.php

--- Resources/Views/components/cores/alert.blade.php
+++ Resources/Views/components/cores/alert.blade.php
@@ -1,27 +1,31 @@
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="container">
+        <div class="alert alert-danger">
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endif
 
 @foreach ($status as $state)
     @if(session()->has($state))
-      <div class="alert alert-{{ $state }}">
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <ul>
-          @foreach (session()->get($state) as $item)
-            <li>{!! $item !!}</li>
-          @endforeach
-        </ul>
-      </div>
+        <div class="container">
+          <div class="alert alert-{{ $state }}">
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <ul>
+              @foreach (session()->get($state) as $item)
+                <li>{!! $item !!}</li>
+              @endforeach
+            </ul>
+          </div>
+        </div>
     @endif
 @endforeach
