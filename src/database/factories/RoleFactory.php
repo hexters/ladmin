942c26af2ece1080fc721f6b18f1d96818127075
saifullah <hexters@gmail.com>
2020-10-25 00:11:48 +0700

    fix RoleFactory

--- src/database/factories/RoleFactory.php
+++ src/database/factories/RoleFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ModelFactory extends Factory
+class RoleFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
