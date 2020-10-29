cfc1b3d49476fdbf2d6200064b16fb1f425b8da0
saifullah <hexters@gmail.com>
2020-10-30 05:19:32 +0700

    notification list

--- Resources/Views/notification/index.blade.php
+++ Resources/Views/notification/index.blade.php
@@ -12,13 +12,15 @@
       <div class="tracking-content">
         <a href="javascript:void(0);" data-link="{{ $item->link }}" data-id="{{ $item->id }}" class="ladmin-notification-link">
           <x-ladmin-card class="mb-0">
-            @if(!is_null($item->image_link))
-              <img src="{{ $item->image_link }}" class="mr-3" width="50">
-            @endif
-            <div class="media-body ladmin-substr">
-              <small class="text-muted {{ is_null($item->read_at) ? 'font-weight-bold' : '' }} float-right">{{ $item->created_at->diffForHumans() }}</small>
-              <strong class="mt-0 mb-1 text-dark">{{ $item->title }}</strong>
-              <p class="m-0 text-muted">{!! $item->description !!}</p>
+            <div class="media">
+              @if(!is_null($item->image_link))
+                <img src="{{ $item->image_link }}" class="mr-3" width="50">
+              @endif
+              <div class="media-body ladmin-substr">
+                <small class="text-muted {{ is_null($item->read_at) ? 'font-weight-bold' : '' }} float-right">{{ $item->created_at->diffForHumans() }}</small>
+                <strong class="mt-0 mb-1 text-dark">{{ $item->title }}</strong>
+                <p class="m-0 text-muted">{!! $item->description !!}</p>
+              </div>
             </div>
           </x-ladmin-card>
         </a>
