9da154d11726f3d74604a6572a9332276cac7074
saifullah <hexters@gmail.com>
2020-11-28 12:00:52 +0700

    fix bug logable model

--- src/Models/LadminLogable.php
+++ src/Models/LadminLogable.php
@@ -13,6 +13,8 @@ class LadminLogable extends Model {
         'user_id',
         'new_data',
         'old_data',
+        'logable_type',
+        'logable_id',
         'type',
         'state'
     ];
